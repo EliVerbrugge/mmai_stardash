90c54e0527e077d2eb688e0f86fc8f7556358cb9
SMst00100T mst00100 <mst00100@rc25xcs213.managed.mst.edu>
2019-04-13 20:16:09 -0500

    distances and MinByValue

--- Games/StarDash/Extensions.cs
+++ Games/StarDash/Extensions.cs
@@ -16,5 +16,40 @@ namespace Joueur.cs.Games.Stardash
         {
             return Solver.distance(unit.X - body.X, unit.Y - body.Y);
         }
+        public static double distance(this Unit unit, Unit unit2)
+        {
+            return Solver.distance(unit.X - unit2.X, unit.Y - unit2.Y);
+        }
+        public static double distance(this Body body, Body body2)
+        {
+            return Solver.distance(body.X - body2.X, body.Y - body2.Y);
+        }
+        public static double distance(this Body body, Unit unit)
+        {
+            return Solver.distance(body.X - unit.X, body.Y - unit.Y);
+        }
+
+        public static T MinByValue<T, K>(this IEnumerable<T> source, Func<T, K> selector)
+        {
+            var comparer = Comparer<K>.Default;
+
+            var enumerator = source.GetEnumerator();
+            enumerator.MoveNext();
+
+            var min = enumerator.Current;
+            var minV = selector(min);
+
+            while (enumerator.MoveNext())
+            {
+                var s = enumerator.Current;
+                var v = selector(s);
+                if (comparer.Compare(v, minV) < 0)
+                {
+                    min = s;
+                    minV = v;
+                }
+            }
+            return min;
+        }
     }
 }
